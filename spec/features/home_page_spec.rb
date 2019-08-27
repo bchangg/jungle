a4852339f8cc9f44af898f30a07449025fb4a41f
Brian Chang <changbhc@gmail.com>
2019-08-27 19:22:05 +0000

    ran bundle update, generated home_page_spec.rb

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,13 +1,5 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 
-RSpec.feature 'Visitor navigates to home page', type: :feature, js: true do
-  scenario 'They see all products' do
-    # ACT
-    visit root_path
-
-    # DEBUG / VERIFY
-    save_screenshot
-  end
+RSpec.feature "HomePages", type: :feature do
+  pending "add some scenarios (or delete) #{__FILE__}"
 end
